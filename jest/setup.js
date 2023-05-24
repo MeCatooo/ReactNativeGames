b312582c706bd49192de5479c9102995ed3f3891
MeCatooo <adam.turek@microsoft.wsei.edu.pl>
2023-05-24 10:31:57 +0200

    Integrate with jest tests

---
+++ jest/setup.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.useFakeTimers();
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
